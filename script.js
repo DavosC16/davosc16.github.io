dbf71f8498c642549ef73d9486be708b77ff1744
DavosC16 <davidcortes1608@gmail.com>
2024-01-09 16:28:47 -0600

    Primer Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var height = window.innerHeight;
+var width = window.innerWidth;
+console.log(height, width);
+
+function noClick(){ 
+    var x;
+    var y;
+
+    do{
+        x = this.width * 0.8 * Math.random();
+    }while(x>this.width)
+
+    do{
+        y = this.height * 0.8 * Math.random();
+    }while(y>this.height)
+
+    document.getElementById("noButton").style.position = "absolute";
+    document.getElementById("noButton").style.left = x + "px";
+    document.getElementById("noButton").style.top= y + "px";
+}
+
+function yesClick(){
+    window.location.href="yes.html";
+}
