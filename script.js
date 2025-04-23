b18fc315e37013183acbfb012aba59922e43bc1c
DavosC16 <davidcortes1608@gmail.com>
2025-04-23 16:27:36 -0600

    Error en IP

--- script.js
+++ script.js
@@ -13,32 +13,51 @@ function noClick(){
         console.log(ip.ip);
         
         //let newURL = "https://ipapi.co/" + ip.ip + "/json/";
-        let newURL="https://ip-api.com/json/"+ip.ip;
+        //let newURL="https://ip-api.com/json/"+ip.ip;
+        let newURL="https://ipapi.co/"+ip.ip+"/json/"
         console.log(newURL);
 
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             console.log(this.responseText);
-           let data = JSON.parse(this.responseText);
-           
-           let newHTML = '<h1 class="header_text">You have been hacked</h1>\n\n';
+            let data = JSON.parse(this.responseText);
+            
+            let newHTML = '<h1 class="header_text">You have been hacked</h1>\n\n';
 
-           newHTML = newHTML + '<div><img src ="https://i.pinimg.com/originals/02/e9/43/02e943d3a936334cfc94aca64dc333e1.gif"><\div>'
+            newHTML = newHTML + '<div><img src ="https://i.pinimg.com/originals/02/e9/43/02e943d3a936334cfc94aca64dc333e1.gif"><\div>'
 
-            newHTML = newHTML + '<p>ip: ' + data.query + '</p>\n';
-            newHTML = newHTML + '<p>country: ' + data.country + '</p>\n';
-            newHTML = newHTML + '<p>region: ' + data.regionName + '</p>\n';
+            newHTML = newHTML + '<p>ip: ' + data.ip + '</p>\n';
+            newHTML = newHTML + '<p>country: ' + data.country_name + '</p>\n';
+            newHTML = newHTML + '<p>region: ' + data.region + '</p>\n';
             newHTML = newHTML + '<p>city: ' + data.city + '</p>\n';
-            newHTML = newHTML + '<p>lat: ' + data.lat + ' N</p>\n';
-            newHTML = newHTML + '<p>lon: ' + data.lon + ' W</p>\n';
+            newHTML = newHTML + '<p>lat: ' + data.latitude + ' N</p>\n';
+            newHTML = newHTML + '<p>lon: ' + data.longitude + ' W</p>\n';
 
             console.log(newHTML);
 
             document.getElementById("container").innerHTML=newHTML;
         };
+
+        xhr.onerror=function(){            
+            let newHTML = '<h1 class="header_text">You have been hacked</h1>\n\n';
+
+            newHTML = newHTML + '<div><img src ="https://i.pinimg.com/originals/02/e9/43/02e943d3a936334cfc94aca64dc333e1.gif"><\div>'
+
+            let nose = 'no se';
+            newHTML = newHTML + '<p>ip: ' + nose + '</p>\n';
+            newHTML = newHTML + '<p>country: ' + nose + '</p>\n';
+            newHTML = newHTML + '<p>region: ' + nose + '</p>\n';
+            newHTML = newHTML + '<p>city: ' + nose + '</p>\n';
+            newHTML = newHTML + '<p>lat: ' + nose + '</p>\n';
+            newHTML = newHTML + '<p>lon: ' + nose + '</p>\n';
+
+            console.log(newHTML);
+
+            document.getElementById("container").innerHTML=newHTML;
+        }
+
         xhr.open('GET', newURL, true);
         xhr.send();
-        
     }
     )
     .catch("ERROR");    
